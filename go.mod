8eff64fb56682d5b3504974e049bfb61f0fa5b9e
thisisaaronland <aaron@localhost>
2021-04-28 11:26:22 -0700

    update to whosonfirst/go-whosonfirst-export v2.3.0, vendor deps

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/whosonfirst/go-whosonfirst-writer
 go 1.16
 
 require (
-	github.com/tidwall/gjson v1.6.8
-	github.com/whosonfirst/go-whosonfirst-export/v2 v2.1.0
-	github.com/whosonfirst/go-whosonfirst-geojson-v2 v0.15.1
+	github.com/tidwall/gjson v1.7.5
+	github.com/whosonfirst/go-whosonfirst-export/v2 v2.3.0
+	github.com/whosonfirst/go-whosonfirst-geojson-v2 v0.16.3
 	github.com/whosonfirst/go-whosonfirst-uri v0.2.0
 	github.com/whosonfirst/go-writer v0.4.1
 )
--- vendor/github.com/whosonfirst/go-whosonfirst-export/v2/properties/hierarchy.go
+++ vendor/github.com/whosonfirst/go-whosonfirst-export/v2/properties/hierarchy.go
@@ -30,6 +30,15 @@ func EnsureHierarchy(feature []byte) ([]byte, error) {
 
 	key := fmt.Sprintf("%s_id", pt)
 
+	if pt == "custom" {
+
+		alt_rsp := gjson.GetBytes(feature, "properties.wof:placetype_alt")
+
+		if alt_rsp.Exists() {
+			key = fmt.Sprintf("%s_id", alt_rsp.String())
+		}
+	}
+
 	hierarchies := make([]Hierarchy, 0)
 
 	hier_rsp := gjson.GetBytes(feature, "properties.wof:hierarchy")
@@ -41,6 +50,22 @@ func EnsureHierarchy(feature []byte) ([]byte, error) {
 			h := make(map[string]int64)
 
 			for k, r := range possible.Map() {
+
+				if k == "custom_id" {
+
+					alt_rsp := gjson.GetBytes(feature, "properties.wof:placetype_alt")
+
+					if alt_rsp.Exists() {
+						k = fmt.Sprintf("%s_id", alt_rsp.String())
+					}
+				}
+
+				v, exists := h[k]
+
+				if exists {
+					return nil, fmt.Errorf("Hierarchy key '%s' already set with value '%d' (trying to set '%d')", k, v, r.Int())
+				}
+
 				h[k] = r.Int()
 			}
 
